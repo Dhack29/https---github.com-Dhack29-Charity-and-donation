1b59f710c970b927e8f070d5d1076e355e4f876b
Dhack29 <dipek2912@gmail.com>
2023-11-24 21:32:42 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import page from "./page"
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
